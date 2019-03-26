3ca35b0c73721407fe0a84fca42d0166129c3a70
nitheeshjoseph <48926665+nitheeshjoseph@users.noreply.github.com>
2019-03-26 13:08:20 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,126 @@
+(function($) {
+
+    "use strict";
+	
+    $(document).ready(function() {
+		
+		// PRELOADER
+        $("body").toggleClass("loaded");
+        setTimeout(function() {
+            $("body").addClass("loaded");
+        }, 3000);
+		
+		// PORTFOLIO DIRECTION AWARE HOVER EFFECT
+		var item = $("#bl-work-items>div");
+		var elementsLength = item.length;
+		for (var i = 0; i < elementsLength; i++) {
+			$(item[i]).hoverdir();
+		}
+		
+		// TEXT ROTATOR
+		$("#selector").animatedHeadline({
+             animationType: "clip"
+        });
+		
+		// BOX LAYOUT
+        Boxlayout.init();
+		
+		// REMOVE # FROM URL
+		$("a[href='#']").on("click", (function(e) {
+			e.preventDefault();
+		}));
+
+        // AJAX CONTACT FORM
+        $(".contactform").on("submit", function() {
+            $(".output_message").text("Loading...");
+
+            var form = $(this);
+            $.ajax({
+                url: form.attr("action"),
+                method: form.attr("method"),
+                data: form.serialize(),
+                success: function(result) {
+                    if (result == "success") {
+                        $(".contactform").find(".output_message").addClass("success");
+                        $(".output_message").text("Message Sent!");
+                    } else {
+                        $(".contactform").find(".output_message").addClass("error");
+                        $(".output_message").text("Error Sending!");
+                    }
+                }
+            });
+
+            return false;
+        });
+
+		// MATERIAL CAROUSEL
+        $(".carousel.carousel-slider").carousel({
+            fullWidth: true,
+            indicators: true,
+        });
+		
+		// RESUME CARDS ANIMATION
+		if ($(window).width() > 800) {
+			$(".resume-list-item, .resume-card").on("click", function() {
+				$(".resume-list-item").removeClass("is-active");
+				var e = parseInt($(this).data("index"),10);
+				$("#resume-list-item-" + e).addClass("is-active");
+				var t = e + 1,
+					n = e - 1,
+					i = e - 2;
+				$(".resume-card").removeClass("front back up-front up-up-front back-back"), $(".resume-card-" + e).addClass("front"), $(".resume-card-" + t).addClass("back"), $(".resume-card-" + n).addClass("back-back"), $(".resume-card-" + i).addClass("back")
+			});
+		}
+		
+    });
+
+})(jQuery);
+
+
+
+// PORTFOLIO RESPONSIVE TAB
+// tabbed content
+    // http://www.entheosweb.com/tutorials/css/tabs.asp
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
+
+  /* if in tab mode */
+    $("ul.tabs-left li").click(function() {
+		
+      $(".tab_content").hide();
+      var activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn();		
+		
+      $("ul.tabs-left li").removeClass("active");
+      $(this).addClass("active");
+
+	  $(".tab_drawer_heading").removeClass("d_active");
+	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+	  
+    /*$(".tabs-left").css("margin-top", function(){ 
+       return ($(".tab_container").outerHeight() - $(".tabs-left").outerHeight() ) / 2;
+    });*/
+    });
+    $(".tab_container").css("min-height", function(){ 
+      return $(".tabs-left").outerHeight() + 50;
+    });
+	/* if in drawer mode */
+	$(".tab_drawer_heading").click(function() {
+      
+      $(".tab_content").hide();
+      var d_activeTab = $(this).attr("rel"); 
+      $("#"+d_activeTab).fadeIn();
+	  
+	  $(".tab_drawer_heading").removeClass("d_active");
+      $(this).addClass("d_active");
+	  
+	  $("ul.tabs-left li").removeClass("active");
+	  $("ul.tabs-left li[rel^='"+d_activeTab+"']").addClass("active");
+    });
+	
+	
+	/* Extra class "tab_last" 
+	   to add border to bottom side
+	   of last tab 
+	$('ul.tabs-left li').last().addClass("tab_last");*/
+	
---
+++ php/process-form.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_REQUEST['name'],$_REQUEST['email'])) {
+      
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $phone = $_REQUEST['phone'];
+    $message = $_REQUEST['message'];
+      
+    // Set your email address where you want to receive emails. 
+    $to = 'nitheeshjoseph90@gmail.com';
+      
+    $subject = 'Contact Request From Portfolio Website';
+    $headers = "From: ".$name." <".$email."> \r\n";
+      
+    $send_email = mail($to,$subject,$message,$headers);
+      
+    echo ($send_email) ? 'success' : 'error';
+      
+}
+?>
